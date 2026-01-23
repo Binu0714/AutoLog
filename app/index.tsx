693bfdba1241a8b4205665f90dfd445f359728c6
Binu0714 <binuj714@gmail.com>
2026-01-23 21:28:40 +0530

    create project structure

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import "../global.css"
+import React from 'react'
+import { View, Text } from 'react-native'
+
+ 
+function Index() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-3xl font-bold">
+        Hello World
+      </Text>
+    </View>
+  )
+}
+
+export default Index
