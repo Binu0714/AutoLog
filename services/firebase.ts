a63b559eeeb9f703350dc3dd66230d4ae5bc7980
Binu0714 <binuj714@gmail.com>
2026-01-29 16:39:36 +0530

    update db configurations

--- services/firebase.ts
+++ services/firebase.ts
@@ -1,11 +1,9 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// @ts-ignore
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.ApiKey,
   authDomain: process.env.AuthDomain,
@@ -16,6 +14,10 @@ const firebaseConfig = {
   measurementId: process.env.MeasurementId
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage)
+});
+
+export const db = getFirestore(app);
